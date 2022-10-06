8f2d035884c392379eaa3996c92846210eee8ee6
Valeriya Sazonova <sazonivaleri@yandex.ru>
2022-10-06 09:47:49 +0300

    add src

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("количество начисленных миль за билет");
+        int ticketPrice = 3000;
+        int rublesForMile = 20;
+        System.out.println(ticketPrice / rublesForMile);
+
+    }
+}
